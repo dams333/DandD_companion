c97589eed9abd3458a316ac40f90a3ae48819f15
Damien <hubleur.damien333@protonmail.ch>
2023-09-23 15:45:50 +0200

    create basic page

---
+++ srcs/App.styles.ts
@@ -0,0 +1,25 @@
+export default styles = {
+	fullHeight: {
+		height: '100%',
+	},
+	container: {
+		padding: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	title: {
+		fontSize: 40,
+		fontFamily: 'DragonHunter',
+	},
+	button: {
+		marginTop: 20,
+		padding: 7,
+		borderRadius: 10,
+		backgroundColor: '#1E90FF',
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 20,
+	},
+};
